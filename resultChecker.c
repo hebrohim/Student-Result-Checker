33c1fa4487fd89dc41cfd9923fa76c8764cc1d94
Hebrohim <raheemibrohim@gmail.com>
2026-07-24 11:36:58 +0100

    add session and semester

--- resultChecker.c
+++ resultChecker.c
@@ -1,1 +1,19 @@
 
+
+
+#include<stdio.h>
+char session[];
+char semester[];
+int main(){
+
+printf("=====================================================================\n");
+printf("                   STUDENT RESULT MANAGEMENT SYSTEM                  ");
+printf("\n=====================================================================\n");
+printf("Academic Session : ");
+scanf("%s",&session);
+printf("Semester         : ");
+scanf("%s",&semester);
+
+
+return 0;
+}
